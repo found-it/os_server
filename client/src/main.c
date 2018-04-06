d99999dbdd55e613f9bf5eaccc02cd5bf5e35a3c
James <jp953310@ohio.edu>
2018-04-06 16:41:21 -0400

    switching computers

--- client/src/main.c
+++ client/src/main.c
@@ -23,6 +23,31 @@
  */
 int main(int argc, char **argv)
 {
+    int readfd;
+    int writefd;
+    mode_t mode = S_IRUSR | S_IWUSR;
+    char recv_pipe[28];
+    char send_pipe[25];
+    snprintf(recv_pipe, 28, "./pipes/%d_receive", getpid());
+    snprintf(send_pipe, 25, "./pipes/%d_send", getpid());
+
+    printf("client receive pipe: %s\n", recv_pipe);
+    printf("client receive pipe: %s\n", send_pipe);
+
+#if 0
+
+    if (mkfifo(recv_pipe, mode) < 0 && (errno != EEXIST))
+    {
+        fprintf(stderr, "Error creating %s FIFO\n", recv_pip);
+        exit(ERROR);
+    }
+
+    if (mkfifo(SRV_WRITE, mode) < 0 && (errno != EEXIST))
+    {
+        fprintf(stderr, "Error creating server write FIFO\n");
+        exit(ERROR);
+    }
+#endif
     struct message msg;
     int wfd = open(SRV_READ, O_WRONLY);
     msg.type = CONNECT;
